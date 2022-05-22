3b9c5440df43ca00d25e9bd644eb0d2a9fdb732d
ashikmahmud <ashikmamud187@gmail.com>
2022-05-23 04:58:05 +0600

    create endpoint to issues token from client

--- Controllers/login.controller.js
+++ Controllers/login.controller.js
@@ -1,7 +1,11 @@
+const jwt = require('jsonwebtoken');
 const loginController= async(req, res) =>{
     const data = req.body;
-    console.log(data);
     
+    const token = jwt.sign(data, process.env.ACCESS_TOKEN, {
+        expiresIn: '1d'
+    })
+    res.send({token})
 }
 
 module.exports ={loginController}
