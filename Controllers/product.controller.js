2b9c6d532936bd4e9010199e67dcb49ea22598bb
ashikmahmud <ashikmamud187@gmail.com>
2022-05-23 22:37:33 +0600

    yah

--- Controllers/product.controller.js
+++ Controllers/product.controller.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require("mongodb");
 const client = require("../Connection/connection");
 const productCollection = client.db("tools-manufactures").collection("products")
 
@@ -17,6 +18,8 @@ const saveProductData = async (req,res) =>{
 }
 
 const getProductData = async (req, res) => {
+   await client.connect();
+
     const uid = req.query.uid;
     const decodedID = req.decoded.uid;
     if(decodedID === uid){
@@ -27,4 +30,22 @@ const getProductData = async (req, res) => {
     }
 }
 
-module.exports = {saveProductData, getProductData}
+
+const deleteProductData = async (req, res) => {
+   await client.connect();
+
+    const uid = req.query.uid;
+    const decodedID = req.decoded.uid;
+    const dltId = req.query.deleteId;
+    if(decodedID === uid){
+        const query ={_id: ObjectId(dltId)}
+        const result = await productCollection.deleteOne(query);
+        if(result.acknowledged){
+            res.send({success: true, message: "Product deleted successfully"})
+        }
+    }else{
+        res.status(403).send({success: false, message:"forbidden request"})
+    }
+}
+
+module.exports = {saveProductData, getProductData, deleteProductData}
--- Routes/product.route.js
+++ Routes/product.route.js
@@ -1,9 +1,10 @@
 
-const { saveProductData, getProductData } = require('../Controllers/product.controller');
+const { saveProductData, getProductData, deleteProductData } = require('../Controllers/product.controller');
 const VerifyAdmin = require('../VerifyAdmin/VerifyAdmin');
 const VerifyToken = require('../VerifyToken/VerifyToken');
 
 const router = require('express').Router();
 router.post('/', VerifyToken, VerifyAdmin, saveProductData);
 router.get('/', VerifyToken, VerifyAdmin, getProductData);
+router.delete('/', VerifyToken, VerifyAdmin, deleteProductData);
 module.exports = router;
