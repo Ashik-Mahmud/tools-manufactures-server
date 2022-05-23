a9be09a6ce63681e63af014f927b97879a80c836
ashikmahmud <ashikmamud187@gmail.com>
2022-05-24 04:35:07 +0600

    added endpoints to showing order data on client with condition

--- Controllers/order.controller.js
+++ Controllers/order.controller.js
@@ -4,10 +4,16 @@ const SendEmail = require("../SendEmail/SendEmail");
 const orderCollection = client.db("tools-manufactures").collection("orders")
 
 const saveOrderData = async(req, res) => {
+    await client.connect();
     const userId = req.query.uid;
     const decodedID = req.decoded.uid;
     if(decodedID === userId){
         const orderData = req.body;
+        const productName = req.body.productName;
+        const isHas = await orderCollection.findOne({productName: productName})
+        if(isHas){
+            return res.send({success: false, message: 'Already ordered this product.'})
+        }
         const result = await orderCollection.insertOne(orderData);
         if(result.acknowledged){
             res.send({success: true, message: 'Order Placed successfully done. Check Your Email.'})
@@ -19,4 +25,18 @@ const saveOrderData = async(req, res) => {
     
 }
 
-module.exports = {saveOrderData}
+const getOrderData = async(req, res) => {
+    await client.connect();
+    const userId = req.query.uid;
+    const decodedID = req.decoded.uid;
+    if(userId === decodedID){
+        const query = {"author.uid": userId};
+        const result = await orderCollection.find(query).toArray();
+        res.send({success: true, result})
+    }else{
+        res.status(403).send({success: false, message: 'forbidden request'})
+    }
+  
+}
+
+module.exports = {saveOrderData,getOrderData}
--- Routes/order.route.js
+++ Routes/order.route.js
@@ -1,8 +1,9 @@
 
-const { saveOrderData } = require('../Controllers/order.controller');
+const { saveOrderData , getOrderData} = require('../Controllers/order.controller');
 
 const VerifyToken = require('../VerifyToken/VerifyToken');
 const router = require('express').Router();
 router.post('/', VerifyToken, saveOrderData);
+router.get('/', VerifyToken, getOrderData);
 
 module.exports = router;
