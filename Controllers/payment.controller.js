91e586db5be2cf95006225a1a6695d057577256c
ashikmahmud <ashikmamud187@gmail.com>
2022-05-24 07:23:29 +0600

    create and endpoint to get payment history: g

--- Controllers/payment.controller.js
+++ Controllers/payment.controller.js
@@ -1,3 +1,6 @@
+const client = require("../Connection/connection");
+const paymentCollection = client.db('tools-manufactures').collection('payments')
+
 const stripe = require('stripe')(process.env.SECRET_STRIPE_KEY);
 const createPaymentIntent = async(req, res) =>{
     const data = req.body;
@@ -16,4 +19,19 @@ const createPaymentIntent = async(req, res) =>{
     }  
 }
 
-module.exports = {createPaymentIntent}
+
+
+const getPaymentHistory = async (req, res) => {
+    await client.connect();
+    const userId = req.query.uid;
+    const decodedID = req.decoded.uid;
+    if(userId === decodedID){
+        const query = {"author.uid": userId}
+        const result = await paymentCollection.find(query).toArray();
+        res.send({success: true, data: result})
+    }else{
+        res.status(403).send({success: false, message: "forbidden Request"})
+    }
+}
+
+module.exports = {createPaymentIntent, getPaymentHistory}
--- Routes/payment.route.js
+++ Routes/payment.route.js
@@ -1,9 +1,10 @@
 
 
-const { createPaymentIntent } = require('../Controllers/payment.controller');
+const { createPaymentIntent, getPaymentHistory } = require('../Controllers/payment.controller');
 const VerifyToken = require('../VerifyToken/VerifyToken');
 
 const router = require('express').Router();
 router.post('/create-payment-intent', VerifyToken, createPaymentIntent);
+router.get('/history', VerifyToken, getPaymentHistory);
 
 module.exports = router;
